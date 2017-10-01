51cc4e249426c8813a1762d3a80801e9db40f75f
bryanh210 <tuongh@sas.upenn.edu>
2017-10-01 01:18:04 -0700

    Initial config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAB3GgQOBnEBAIjcxaOsJt5qjeUyunO9HhpD8QY9sfRPZC5cttIjaZCZA1KllXwmQGs8zyEZBRbTwZCaNQqzWxNlVVI21PYlHc6ZAYm0Hol9FStHG9ithXF1mzZA1a7xmZAtXbC8qP3Xwf4QBexXUHwEpgJY4ZAGi5hahMGRW0p1zDwZDZD',
+    FB_VERIFY_TOKEN: 'sheltertechbot1992',
+    API_AI_CLIENT_ACCESS_TOKEN: '9f82299af71444de8dc9db50332a8f1e',
+    FB_APP_SECRET: '7b99bf9fdc6bcb5ccd9ac78569302d94',
+    SERVER_URL: "https://sheltertechbot.herokuapp.com/",
 };
